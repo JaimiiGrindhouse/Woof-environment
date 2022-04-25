3431969111ce0e9f97da275741bfc4c7be1e9763
carlos alberto triana brigante <brigantetca@gmail.com>
2022-04-25 15:41:11 +0100

    filtered parks rendering on the front-end (no dropdown)

--- docker-recipes/woof_db/app/views/script.js
+++ docker-recipes/woof_db/app/views/script.js
@@ -10,14 +10,22 @@ function areaClick(areaSelection){
 
 function filterParks(parkObject){
     var filteredParks = [];
+    var dropdownList = '<ul>'
     for(let i=0; i<parkObject.length; i++){
         if(parkObject[i].area_name == area){
             filteredParks.push(parkObject[i].park_name)
         }
     }
-    filtered = filteredParks;
-    console.log(filtered)
+    for(let i=0; i<filteredParks.length; i++){
+        dropdownList +='<li>' + filteredParks[i] + '</li>'
+    }
+    dropdownList += '</ul>'
+    console.log(dropdownList)
+    document.getElementById("test").innerHTML = dropdownList;
 }
+
+
+
 
 
 
